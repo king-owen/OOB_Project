609607029a95be7ad0a9b6944163b204fbc489e3
king-owen <owenanderchek@gmail.com>
2020-10-28 19:04:10 -0700

    Update maze.py

--- maze.py
+++ maze.py
@@ -1,4 +1,20 @@
+import csv
+
 class Maze:
         def __init__(self,filename):
-        with open(filename, 'r') as inputfile:
-            self.content = inputfile.readlines()
+            with open(filename, 'r') as inputfile:
+                content = list(csv.reader(inputfile))
+            self.content = content
+
+        def check(self, line, column):
+            pass
+
+        def display(self):
+            print(self.content)
+
+        def find_random_spot(self):
+            pass
+
+maze1 = Maze("7X7 Maze.txt")
+
+maze1.display()
