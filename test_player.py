199b2666ced8c517d5ec3c724a8e2a7e59fb6e1b
brent1052 <62719074+brent1052@users.noreply.github.com>
2020-11-04 22:40:54 -0800

    Update test_player.py

--- test_player.py
+++ test_player.py
@@ -1,4 +1,14 @@
 import pytest
 from player import Player
+#
+# @pytest.fixture
 
-@pytest.fixture
+
+
+def test_bag(Player):
+    #checks to see if Player class has the attribute 'backpack'
+    assert hasattr(Player,'backpack')
+
+def test_pickup(Player):
+    #checks to see if Player class has the method appendItem
+    assert hasattr(Player,'appendItem')
