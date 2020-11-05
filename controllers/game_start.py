f7ff81a5b477428f38c51931b31c5386025c076d
king-owen <owenanderchek@gmail.com>
2020-11-04 20:44:41 -0800

    Applied a working movement controller

--- controllers/game_start.py
+++ controllers/game_start.py
@@ -1,63 +1,68 @@
 from models.maze import Maze
 from models.player import Player
+from .game_move import GameMove
 
 class GameStart:
 
     def __init__(self, maze):
         self.maze = Maze(maze)
+        self.running = True
 
     def run(self):
-        running = True
 
         self.maze.display()
 
-        while running == True:
-            direction = input("Enter a direction: ")
+        moving = GameMove(self.maze)
 
-            if direction == "w":
-                if self.maze.can_move_to(self.maze.location[0]-1,self.maze.location[1]) == True:
-                    self.maze.content[self.maze.location[0]][self.maze.location[1]] = " "
-                    self.maze.location = (self.maze.location[0]-1,) + self.maze.location[1:]
-                    print("Successful Move")
-                else:
-                    print("Unsuccessful Move")
+        while self.running == True:
+            moving.move()
 
-            elif direction == "a":
-                if self.maze.can_move_to(self.maze.location[0],self.maze.location[1]-1) == True:
-                    self.maze.content[self.maze.location[0]][self.maze.location[1]] = " "
-                    self.maze.location = (self.maze.location[0], self.maze.location[1]-1,)
-                    print("Successful Move")
-                else:
-                    print("Unsuccessful Move")
+            # direction = input("Enter a direction: ")
+
+            # if direction == "w":
+            #     if self.maze.can_move_to(self.maze.location[0]-1,self.maze.location[1]) == True:
+            #         self.maze.content[self.maze.location[0]][self.maze.location[1]] = " "
+            #         self.maze.location = (self.maze.location[0]-1,) + self.maze.location[1:]
+            #         print("Successful Move")
+            #     else:
+            #         print("Unsuccessful Move")
+
+            # elif direction == "a":
+            #     if self.maze.can_move_to(self.maze.location[0],self.maze.location[1]-1) == True:
+            #         self.maze.content[self.maze.location[0]][self.maze.location[1]] = " "
+            #         self.maze.location = (self.maze.location[0], self.maze.location[1]-1,)
+            #         print("Successful Move")
+            #     else:
+            #         print("Unsuccessful Move")
                 
-            elif direction == "s":
-                if self.maze.can_move_to(self.maze.location[0]+1,self.maze.location[1]) == True:
-                    self.maze.content[self.maze.location[0]][self.maze.location[1]] = " "
-                    self.maze.location = (self.maze.location[0]+1,) + self.maze.location[1:]
-                    print("Successful Move")
-                else:
-                    print("Unsuccessful Move")
+            # elif direction == "s":
+            #     if self.maze.can_move_to(self.maze.location[0]+1,self.maze.location[1]) == True:
+            #         self.maze.content[self.maze.location[0]][self.maze.location[1]] = " "
+            #         self.maze.location = (self.maze.location[0]+1,) + self.maze.location[1:]
+            #         print("Successful Move")
+            #     else:
+            #         print("Unsuccessful Move")
 
-            elif direction == "d":
-                if self.maze.can_move_to(self.maze.location[0],self.maze.location[1]+1) == True:
-                    self.maze.content[self.maze.location[0]][self.maze.location[1]] = " "
-                    self.maze.location = (self.maze.location[0], self.maze.location[1]+1,)
-                    print("Successful Move")
-                else:
-                    print("Unsuccessful Move")
-            else:
-                print("Invalid Direction")
+            # elif direction == "d":
+            #     if self.maze.can_move_to(self.maze.location[0],self.maze.location[1]+1) == True:
+            #         self.maze.content[self.maze.location[0]][self.maze.location[1]] = " "
+            #         self.maze.location = (self.maze.location[0], self.maze.location[1]+1,)
+            #         print("Successful Move")
+            #     else:
+            #         print("Unsuccessful Move")
+            # else:
+            #     print("Invalid Direction")
             
-            if self.maze.can_move_to(self.maze.location[0], self.maze.location[1]) == True:
-                if self.maze.is_exit(self.maze.location[0], self.maze.location[1]) == False:
-                    self.maze.content[self.maze.location[0]][self.maze.location[1]] = "P"
+            # if self.maze.can_move_to(self.maze.location[0], self.maze.location[1]) == True:
+            #     if self.maze.is_exit(self.maze.location[0], self.maze.location[1]) == False:
+            #         self.maze.content[self.maze.location[0]][self.maze.location[1]] = "P"
 
                     
-            if self.maze.is_exit(self.maze.location[0],self.maze.location[1]) == True:
-                print("Exit Reached")
-                running = False
+            # if self.maze.is_exit(self.maze.location[0],self.maze.location[1]) == True:
+            #     print("Exit Reached")
+            #     running = False
 
-            if running == True:
-                self.maze.display()
-                print(self.maze.location)
+            # if running == True:
+            #     self.maze.display()
+            #     print(self.maze.location)
         
