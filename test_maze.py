1b09051fecbb0be5c83fa036141b3d2a478a2ad4
brent1052 <62719074+brent1052@users.noreply.github.com>
2020-11-04 22:59:18 -0800

    Update test_maze.py

--- test_maze.py
+++ test_maze.py
@@ -1,2 +1,24 @@
 import pytest
 from maze import Maze
+
+def test_content(Maze):
+    assert hasattr(Maze,'content')
+
+def test_player(Maze):
+    assert hasattr(Maze,'player')
+
+def test_location(Maze):
+    assert hasattr(Maze,'location')
+    assert type(Maze.location) == list
+
+def test_can_move_to(Maze):
+    assert hasattr(Maze,'can_move_to')
+
+    #would need fixture for this one
+
+def test_display(Maze):
+    assert hasattr(Maze,'display')
+    assert None
+
+def find_random_spot(Maze):
+    assert hasattr(Maze,'find_random_spot')
